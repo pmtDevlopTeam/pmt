e8321e60f2e8dbcf94258500a7e28261e1db8cd6
lvp <lvpeng@camelotchina.com>
2018-03-26 16:10:47 +0800

    Initial commit

---
+++ src/main/java/com/camelot/pmt/PmtApplication.java
@@ -0,0 +1,12 @@
+package com.camelot.pmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/camelot/pmt/PmtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.camelot.pmt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PmtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
