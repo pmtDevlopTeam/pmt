11a2c4c2ad6155c1c83dd8b70676c10d71985142
zsf <962551235@qq.com>
2018-04-09 14:16:43 +0800

    RestController

--- src/main/java/com/camelot/pmt/task/controller/TaskController.java
+++ src/main/java/com/camelot/pmt/task/controller/TaskController.java
@@ -1,8 +1,8 @@
 package com.camelot.pmt.task.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller 
+@RestController
 public class TaskController {
 
 }
