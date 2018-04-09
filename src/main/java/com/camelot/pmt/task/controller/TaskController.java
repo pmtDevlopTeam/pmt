6b3f585f656ac8f64f5128f78a6f61f6f3d087ea
zhangao <m15043775855@163.com>
2018-04-09 15:18:24 +0800

    pull

--- src/main/java/com/camelot/pmt/task/controller/TaskController.java
+++ src/main/java/com/camelot/pmt/task/controller/TaskController.java
@@ -1,8 +1,22 @@
 package com.camelot.pmt.task.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+/**
+ * 
+	* @ClassName: TaskController
+	* @Description: TODO
+	* @author zhangao
+	* @date 2018年4月9日
+	*
+ */
 
+import com.camelot.pmt.task.service.TaskService;
 @Controller 
 public class TaskController {
+	
+	@Autowired
+	private TaskService taskService;
 
-}
+}	
+	
