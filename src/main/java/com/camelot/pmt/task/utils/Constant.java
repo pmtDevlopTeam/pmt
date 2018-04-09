d17711e2528c1ad54321703fb0b8f2654aa50385
guoxingliang <838101520@qq.com>
2018-04-09 15:00:01 +0800

    修改类注释

---
+++ src/main/java/com/camelot/pmt/task/utils/Constant.java
@@ -0,0 +1,31 @@
+package com.camelot.pmt.task.utils;
+
+/**
+ * 常量
+ * 
+ */
+public class Constant {
+	/** 超级管理员ID */
+	public static final int SUPER_ADMIN = 1;
+
+	/** 任务类型demo */
+    public enum TaskType {
+        /** 目录   */
+    	CATALOG(0),
+        /** 菜单  */
+        MENU(1),
+        /** 按钮  */
+        BUTTON(2);
+
+        private int value;
+
+        private TaskType(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+}
