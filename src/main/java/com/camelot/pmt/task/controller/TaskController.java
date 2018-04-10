5ed0820a4fac5645780cdebfde3f8058b2e63f93
956772658@qq.com <956772658@qq.com>
2018-04-10 09:37:52 +0800

    修改报错注释

--- src/main/java/com/camelot/pmt/task/controller/TaskController.java
+++ src/main/java/com/camelot/pmt/task/controller/TaskController.java
@@ -2,7 +2,7 @@ package com.camelot.pmt.task.controller;
 
 import java.util.List;
 
-import org.junit.Ignore;
+//import org.junit.Ignore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
