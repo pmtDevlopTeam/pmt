261b2234e59e5dff963eec92b21ee5078b7d36e7
zlh123git <1175272773@qq.com>
2018-04-10 15:52:22 +0800

    zlh

--- src/main/java/com/camelot/pmt/task/mapper/TaskMapper.java
+++ src/main/java/com/camelot/pmt/task/mapper/TaskMapper.java
@@ -1,6 +1,9 @@
 package com.camelot.pmt.task.mapper;
 
+import com.alibaba.fastjson.JSONObject;
 import com.camelot.pmt.task.model.Task;
+
+import java.util.List;
 
 public interface TaskMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +17,20 @@ public interface TaskMapper {
     int updateByPrimaryKeySelective(Task record);
 
     int updateByPrimaryKey(Task record);
+
+    /**
+     * @author: zlh
+     * @param:
+     * @description: 查询所有任务列表
+     * @date: 16:54 2018/4/9
+     */
+    List<Task> queryAllTask();
+
+    /**
+     * @param task
+     *            模糊查询的条件
+     * @description: 根据条件查询任务
+     * @return
+     */
+    List<Task> queryTaskByTask(Task task);
 }
