3c693a559ec94e2dc86af61ca302d4bf98a74bbb
gnerv <34653480+gnerv@users.noreply.github.com>
2018-04-11 17:29:57 +0800

    基础环境调整

--- src/test/java/com/camelot/pmt/PmtApplicationTests.java
+++ src/test/java/com/camelot/pmt/PmtApplicationTests.java
@@ -1,16 +1,38 @@
 package com.camelot.pmt;
 
+import javax.servlet.http.HttpSession;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PmtApplicationTests {
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    
+	@Autowired
+	private RedisTemplate redisTemplate;
+	
+	@Autowired
+	HttpSession session;
+	
 	@Test
 	public void contextLoads() {
+        stringRedisTemplate.opsForValue().set("aaa", "111");
+        String string = stringRedisTemplate.opsForValue().get("aaa");
+        Assert.assertEquals("111", string);
+        System.out.println(string);
+        
+        session.setAttribute("users", "username");
+		
 	}
 
 }
