4e53a8176c327e7cd1f22978246fec7d3d795112
gnerv <34653480+gnerv@users.noreply.github.com>
2018-04-12 16:40:04 +0800

    common

---
+++ src/main/java/com/camelot/pmt/platform/common/CommonAttributeUtil.java
@@ -0,0 +1,22 @@
+package com.camelot.pmt.platform.common;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 创建人 创建时间 修改人 修改时间 通用添加方法
+ * 
+ */
+@Component
+public class CommonAttributeUtil {
+	
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+    public static T addCommonAttribute(<T> t) {
+    	
+        return null;
+    }
+}
