3f571b1d34ec8b00dc4d2f7adb7f020a0ec2486c
956772658@qq.com <956772658@qq.com>
2018-04-12 11:37:59 +0800

    4组分支提交

---
+++ src/main/java/com/camelot/pmt/project/controller/DemandController.java
@@ -0,0 +1,21 @@
+package com.camelot.pmt.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *@Author:fjy
+ *@Description: 需求控制器类
+ *@Date:2018/4/11_11:03
+ */
+@Controller
+@RequestMapping
+public class DemandController {
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/camelot/pmt/project/service/impl/DemandServiceImpl.java
@@ -0,0 +1,6 @@
+package com.camelot.pmt.project.service.impl;
+
+import com.camelot.pmt.project.service.DemandService;
+
+public class DemandServiceImpl implements DemandService {
+}
