e8b0e52cf55e7be3c42c541d88f54d6d0ba3e1e7
zz <1798805612@qq.com>
2018-04-12 14:30:49 +0800

    文件管理功能代码上传

--- src/main/java/com/camelot/pmt/filemanage/service/FileManageGroupService.java
+++ src/main/java/com/camelot/pmt/filemanage/service/FileManageGroupService.java
@@ -1,10 +1,23 @@
 package com.camelot.pmt.filemanage.service;
 
+import com.camelot.pmt.filemanage.model.FileManageGroup;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+
+import java.util.List;
+
 /**
  *
  * FileManageGroup 表数据服务层接口
  *
  */
 public interface FileManageGroupService {
+    Boolean addFileManagerGroup(HttpServletRequest request, FileManageGroup fileManageGroup);//添加文件夹
 
+    Boolean deleteFileGroup(FileManageGroup fileManageGroup);//删除文件夹
+
+    Boolean updateFileGroupById(FileManageGroup fileManageGroup);//修改文件夹
+
+    /*List<FileManageGroup> selectFileGroupByProjectID(FileManageGroup fileManageGroup);*/
+
+    List<FileManageGroup> selectFileGroup(FileManageGroup fileManageGroup);
 }
