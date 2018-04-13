607155893989a1b2eb41cab587d36916f5768573
jh <jh@10.6.60.61>
2018-04-13 17:44:32 +0800

    项目预算模块（新增，查询，编辑）

---
+++ src/main/java/com/camelot/pmt/project/service/ProjectWarningService.java
@@ -0,0 +1,27 @@
+/**
+ * @Package: com.camelot.pmt.project.service 
+ * @author: jh 
+ * @date: 2018年4月12日 下午5:16:34 
+ */
+package com.camelot.pmt.project.service;
+
+import com.camelot.pmt.platform.user.model.UserModel;
+import com.camelot.pmt.platform.utils.DataGrid;
+import com.camelot.pmt.platform.utils.ExecuteResult;
+
+/** 
+ * @ClassName: ProjectWarningService 
+ * @Description: TODO 
+ * @author lixiaokang
+ * @date 2018年4月12日 下午5:16:34  
+ */
+public interface ProjectWarningService {
+
+	/**
+	 * @param param
+	 * @return
+	 */
+	ExecuteResult<String> startProjectWarning(String param);
+
+	
+}
