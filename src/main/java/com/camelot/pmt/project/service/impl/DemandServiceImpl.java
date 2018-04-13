987c76956e974c198e72c48daa931f21c0666014
956772658@qq.com <956772658@qq.com>
2018-04-13 18:27:26 +0800

    提交4组代码

--- src/main/java/com/camelot/pmt/project/service/impl/DemandServiceImpl.java
+++ src/main/java/com/camelot/pmt/project/service/impl/DemandServiceImpl.java
@@ -1,9 +1,68 @@
 package com.camelot.pmt.project.service.impl;
 
+import com.camelot.pmt.platform.utils.DataGrid;
+import com.camelot.pmt.platform.utils.ExecuteResult;
+import com.camelot.pmt.platform.utils.Pager;
+import com.camelot.pmt.project.mapper.DemandMapper;
+import com.camelot.pmt.project.model.DemandWithBLOBs;
 import com.camelot.pmt.project.service.DemandService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class DemandServiceImpl implements DemandService {
+    private static final Logger logger = LoggerFactory.getLogger(DemandServiceImpl.class);
+    @Resource
+    DemandMapper demandMapper;
+    @Override
+    public ExecuteResult<String> save(DemandWithBLOBs demandWithBLOBs) {
+        ExecuteResult<String> result=new ExecuteResult<>();
+        try {
+        if (demandWithBLOBs==null){
+            result.setResultMessage("传入参数不能为空");
+            return result;
+        }
+            demandMapper.insert(demandWithBLOBs);
+            result.setResult("新增需求成功");
+        }catch (Exception e){
+            logger.error("--------新增需求-------"+e.getMessage());
+
+        }
+        return result;
+    }
+
+    /**
+     * 查询需求分页
+     * @param demandWithBLOBs
+     * @return
+     */
+    @Override
+    public ExecuteResult<DataGrid<DemandWithBLOBs>> findAllByPage(Pager pager, DemandWithBLOBs demandWithBLOBs) {
+        ExecuteResult<DataGrid<DemandWithBLOBs>> result=new ExecuteResult<>();
+        try{
+            List<DemandWithBLOBs> demandWithBLOBsList=demandMapper.findAllByPage(pager,demandWithBLOBs);
+            if(CollectionUtils.isEmpty(demandWithBLOBsList)){
+                DataGrid<DemandWithBLOBs> dg = new DataGrid<DemandWithBLOBs>();
+                result.setResult(dg);
+                return result;
+            }
+            DataGrid<DemandWithBLOBs> dg = new DataGrid<DemandWithBLOBs>();
+            dg.setRows(demandWithBLOBsList);
+            //查询总条数
+            Long total = demandMapper.queryCount(demandWithBLOBs);
+            dg.setTotal(total);
+            result.setResult(dg);
+        }catch(Exception e){
+            logger.error("-------需求查询分页------"+e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return result;
+        }
 }
