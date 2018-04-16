806a3a0f0f445e757f19b22a60701c9513adfd24
songliangliang1 <18811152487@163.com>
2018-04-16 11:18:06 +0800

    12

--- src/main/java/com/camelot/pmt/caserepertory/service/CaseRepertoryService.java
+++ src/main/java/com/camelot/pmt/caserepertory/service/CaseRepertoryService.java
@@ -1,5 +1,12 @@
 package com.camelot.pmt.caserepertory.service;
 
+import java.util.Map;
+
+import com.camelot.pmt.platform.utils.ExecuteResult;
+import com.github.pagehelper.PageInfo;
+
 public interface CaseRepertoryService {
+
+	ExecuteResult<PageInfo> selectCondition(Map<String, Object> map);
 
 }
--- src/main/java/com/camelot/pmt/caserepertory/service/impl/CaseRepertoryServiceImpl.java
+++ src/main/java/com/camelot/pmt/caserepertory/service/impl/CaseRepertoryServiceImpl.java
@@ -1,13 +1,23 @@
 package com.camelot.pmt.caserepertory.service.impl;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.camelot.pmt.caserepertory.mapper.CaseRepertoryMapper;
 import com.camelot.pmt.caserepertory.service.CaseRepertoryService;
+import com.camelot.pmt.platform.utils.ExecuteResult;
 import com.camelot.pmt.testmanage.bugmanage.mapper.BugHistoryMapper;
+import com.github.pagehelper.PageInfo;
 @Service
 public class CaseRepertoryServiceImpl implements CaseRepertoryService {
 	@Autowired
 	private CaseRepertoryMapper caseRepertoryMapper;
+
+	@Override
+	public ExecuteResult<PageInfo> selectCondition(Map<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
