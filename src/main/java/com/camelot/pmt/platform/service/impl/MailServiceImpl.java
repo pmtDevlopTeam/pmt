8338cd0d9c36a4ea07c6477d8f6f3ac66871c4b1
gnerv <34653480+gnerv@users.noreply.github.com>
2018-04-17 11:48:28 +0800

    三组 增加邮件和定时任务服务

---
+++ src/main/java/com/camelot/pmt/platform/service/impl/MailServiceImpl.java
@@ -0,0 +1,43 @@
+package com.camelot.pmt.platform.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.camelot.pmt.platform.service.MailService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class MailServiceImpl implements MailService{
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    public void sendSimpleMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("ligendwow@163.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        try {
+            mailSender.send(message);
+            logger.info("简单邮件已经发送。");
+        } catch (Exception e) {
+            logger.error("发送简单邮件时发生异常！", e);
+        }
+
+    }
+
+	@Override
+	public void sendSimpleMail(String to, String subject, String content, String filePath) {
+		// TODO Auto-generated method stub
+		
+	}
+}
