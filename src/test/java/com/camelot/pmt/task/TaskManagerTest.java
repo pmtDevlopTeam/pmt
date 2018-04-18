8a14e1d7a3cd9cb7b4f4b6a9128fe82585e30124
zlh123git <1175272773@qq.com>
2018-04-18 11:12:29 +0800

    taskManage 模块 进行测试修改

--- src/test/java/com/camelot/pmt/task/TaskManagerTest.java
+++ src/test/java/com/camelot/pmt/task/TaskManagerTest.java
@@ -1,6 +1,7 @@
 package com.camelot.pmt.task;
 
 import com.camelot.pmt.common.ExecuteResult;
+import com.camelot.pmt.platform.model.User;
 import com.camelot.pmt.task.model.Task;
 import com.camelot.pmt.task.service.TaskManagerService;
 import org.junit.Test;
@@ -9,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author zlh
@@ -29,5 +32,25 @@ public class TaskManagerTest {
         for (Task task : result) {
             System.out.println(task);
         }
+    }
+    @Test
+    public void testInsertTask() throws Exception {
+        Task task = new Task();
+        User user = new User();
+        user.setUserId("2");
+        task.setBeassignUser(user);
+        task.setTaskType("任务");
+        task.setTaskName("乱七八糟任务");
+        task.setEstimateStartTime(new Date());
+        task.setEstimateEndTime(new Date());
+        task.setTaskDescribe("这是任务描述");
+        ExecuteResult<String> stringExecuteResult = taskManagerService.insertTask(task, null);
+    }
+    @Test
+    public void testQueryTaskById() throws Exception {
+        ExecuteResult<Map<String, Object>> mapExecuteResult = taskManagerService.queryTaskById((long) 2);
+        Map<String, Object> result = mapExecuteResult.getResult();
+        Task task = (Task) result.get("Task");
+        System.out.println(task);
     }
 }
