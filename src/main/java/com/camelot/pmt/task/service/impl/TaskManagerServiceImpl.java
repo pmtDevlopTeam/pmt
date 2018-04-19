f0df6a9381134cd6cd36f55dc68bacb1b448ec70
zlh123git <1175272773@qq.com>
2018-04-19 12:01:34 +0800

    规范

--- src/main/java/com/camelot/pmt/task/service/impl/TaskManagerServiceImpl.java
+++ src/main/java/com/camelot/pmt/task/service/impl/TaskManagerServiceImpl.java
@@ -8,6 +8,9 @@ import com.camelot.pmt.task.model.Task;
 import com.camelot.pmt.task.model.TaskFile;
 import com.camelot.pmt.task.service.TaskFileService;
 import com.camelot.pmt.task.service.TaskManagerService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +34,7 @@ import java.util.Map;
 @Service
 public class TaskManagerServiceImpl implements TaskManagerService {
 
+    // 日志
     private static final Logger LOGGER = LoggerFactory.getLogger(TaskManagerServiceImpl.class);
 
     @Autowired
@@ -39,61 +45,6 @@ public class TaskManagerServiceImpl implements TaskManagerService {
 
     @Autowired
     private TaskMapper taskMapper;
-
-    /**
-     * @author: zlh
-     * @param:
-     * @description: 查询所有任务
-     * @date: 16:54 2018/4/9
-     */
-    @Override
-    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
-    public ExecuteResult<List<Task>> queryAllTask() {
-        ExecuteResult<List<Task>> result = new ExecuteResult<List<Task>>();
-        try {
-            List<Task> tasks = taskMapper.queryAllTask();
-            result.setResult(tasks);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return result;
-    }
-
-    /**
-     * @param task 模糊查询的条件
-     * @author: zlh
-     * @description: 根据条件查询任务
-     */
-    @Override
-    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
-    public ExecuteResult<List<Task>> queryTaskByTask(Task task) {
-        ExecuteResult<List<Task>> result = new ExecuteResult<List<Task>>();
-        try {
-            // 检查参数
-            if (task == null) {
-                result.addErrorMessage("参数有误");
-                return result;
-            }
-            List<String> ids = null;
-            if (task.getBeassignUser() != null) {
-                 /*如果负责人条件非空，则根据username查询userId*/
-                ExecuteResult<List<User>> usersResult = userService.queryUsersByUserName(task.getBeassignUser().getUsername());
-                List<User> users = usersResult.getResult();
-                // 赋值给string数组传给DAO层
-                ids = new ArrayList<String>();
-                for (User user : users) {
-                    ids.add(user.getUserId());
-                }
-            }
-            List<Task> tasks = taskMapper.queryTaskByTask(task, ids);
-            result.setResult(tasks);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return result;
-    }
 
     /**
      * @author: zlh
@@ -118,11 +69,19 @@ public class TaskManagerServiceImpl implements TaskManagerService {
             user.setUserId(userId);
             task.setCreateUser(user);
             int insertTask = taskMapper.insertTask(task);
-            // 如果任务类型为需求任务，上传附件
-            if ("需求".equals(task.getTaskType())) {
+            // 如果任务有附件，上传附件
+            if (file != null) {
                 String fileName = file.getOriginalFilename();
                 byte[] bytes = file.getBytes();
-                /*写出到指定位置*/
+                String filePath = "D:/upload";
+                File targetFile = new File(filePath);
+                if(!targetFile.exists()){
+                    targetFile.mkdirs();
+                }
+                FileOutputStream out = new FileOutputStream(filePath+fileName);
+                out.write(bytes);
+                out.flush();
+                out.close();
 
                 TaskFile taskFile = new TaskFile();
                 // 附件名称
@@ -138,6 +97,112 @@ public class TaskManagerServiceImpl implements TaskManagerService {
             }
 
             result.setResult("插入成功");
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * @author: zlh
+     * @param: id 需要删除的任务的id，isDeleteAll 是否删除子任务
+     * @description: 根据id删除任务（只能删除自己新建的且没有开始的任务，已经指派的任务只能关闭不能删除）
+     * @date: 17:24 2018/4/12
+     */
+    @Override
+    public ExecuteResult<String> deleteTaskById(Long id) {
+        ExecuteResult<String> result = new ExecuteResult<String>();
+        try {
+            // check参数
+            if (id == null) {
+                result.addErrorMessage("参数传入有误");
+                return result;
+            }
+
+            // 检查权限
+            Task task = taskMapper.queryTaskById(id);
+            String createUserName = task.getCreateUser().getUsername();
+            String status = task.getStatus();
+            if (!"当前登录用户名".equals(createUserName)) {
+                /*return 没有权限*/
+                result.setResult("没有权限");
+                return result;
+            }
+            // 已经指派的任务只能关闭不能删除
+            if (task.getBeassignUser() != null) {
+                /*return 不能删除已经指派的任务*/
+                result.setResult("已指派的任务不能删除");
+                return result;
+            }
+            // 已经开始的任务不能删除
+            if ("开始任务状态码".equals(status)) {
+                /*return 已经开始的任务不能删除*/
+                result.setResult("已开始的任务不能删除");
+                return result;
+            }
+
+            // 删除任务
+            taskMapper.deleteTaskById(id);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * @author: zlh
+     * @param: taskManager 任务修改内容
+     * @description: 编辑任务
+     * 权限：只有任务的创建人可以进行编辑；
+     * @date: 17:05 2018/4/13
+     */
+    @Override
+    public ExecuteResult<String> updateTaskByTask(Task task) {
+        ExecuteResult<String> result = new ExecuteResult<String>();
+        try {
+            // check参数
+            if (task == null) {
+                result.addErrorMessage("参数传入有误");
+                return result;
+            }
+
+            // 检查权限
+            Task task2 = taskMapper.queryTaskById(task.getId());
+            String createUserName = task2.getCreateUser().getUsername();
+            if (!"当前登录用户名".equals(createUserName)) {
+                /*return 没有权限*/
+                result.setResult("没有权限");
+                return result;
+            }
+            taskMapper.updateTaskById(task);
+            result.setResult("修改成功");
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * @author: zlh
+     * @param: taskManager 任务修改内容
+     * @description: 需求是否变更
+     * @date: 17:37 2018/4/13
+     */
+    @Override
+    public ExecuteResult<String> updateDemandChangeByTask(Task task) {
+        ExecuteResult<String> result = new ExecuteResult<String>();
+        try {
+            // check参数
+            if (task == null) {
+                result.addErrorMessage("参数传入有误");
+                return result;
+            }
+            taskMapper.updateTaskById(task);
+            result.setResult("修改成功");
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
             throw new RuntimeException(e);
@@ -262,45 +327,60 @@ public class TaskManagerServiceImpl implements TaskManagerService {
 
     /**
      * @author: zlh
-     * @param: id 需要删除的任务的id，isDeleteAll 是否删除子任务
-     * @description: 根据id删除任务（只能删除自己新建的且没有开始的任务，已经指派的任务只能关闭不能删除）
-     * @date: 17:24 2018/4/12
+     * @param: page  当前页
+     * @param: rows  一页有几行
+     * @description: 查询所有任务
+     * @date: 16:54 2018/4/9
      */
     @Override
-    public ExecuteResult<String> deleteTaskById(Long id) {
-        ExecuteResult<String> result = new ExecuteResult<String>();
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public ExecuteResult<PageInfo<Task>> queryAllTask(Integer page, Integer rows) {
+        ExecuteResult<PageInfo<Task>> result = new ExecuteResult<PageInfo<Task>>();
         try {
-            // check参数
-            if (id == null) {
-                result.addErrorMessage("参数传入有误");
-                return result;
-            }
+            PageHelper.startPage(page, rows);
+            List<Task> tasks = taskMapper.queryAllTask();
+            PageInfo<Task> pageInfo = new PageInfo<>(tasks);
+            result.setResult(pageInfo);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
 
-            // 检查权限
-            Task task = taskMapper.queryTaskById(id);
-            String createUserName = task.getCreateUser().getUsername();
-            String status = task.getStatus();
-            if (!"当前登录用户名".equals(createUserName)) {
-                /*return 没有权限*/
-                result.setResult("没有权限");
+    /**
+     * @author: zlh
+     * @param task 模糊查询的条件
+     * @param: page  当前页
+     * @param: rows  一页有几行
+     * @description: 根据条件查询任务
+     * @return
+     */
+    @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+    public ExecuteResult<PageInfo<Task>> queryTaskByTask(Task task, Integer page, Integer rows) {
+        ExecuteResult<PageInfo<Task>> result = new ExecuteResult<PageInfo<Task>>();
+        try {
+            // 检查参数
+            if (task == null) {
+                result.addErrorMessage("参数有误");
                 return result;
             }
-            // 已经指派的任务只能关闭不能删除
+            String[] ids = null;
             if (task.getBeassignUser() != null) {
-                /*return 不能删除已经指派的任务*/
-                result.setResult("已指派的任务不能删除");
-                return result;
+                /*如果负责人条件非空，则根据username查询userId*/
+                ExecuteResult<List<User>> usersResult = userService.queryUsersByUserName(task.getBeassignUser().getUsername());
+                List<User> users = usersResult.getResult();
+                // 赋值给string数组传给DAO层
+                ids =new String[users.size()];
+                for (int i = 0;i<users.size();i++) {
+                    ids[i] = users.get(i).getUserId();
+                }
             }
-            // 已经开始的任务不能删除
-            if ("开始任务状态码".equals(status)) {
-                /*return 已经开始的任务不能删除*/
-                result.setResult("已开始的任务不能删除");
-                return result;
-            }
-
-            // 删除任务
-            taskMapper.deleteTaskById(id);
-
+//            PageHelper.startPage(page, rows);
+            List<Task> tasks = taskMapper.queryTaskByTask(task, ids);
+            PageInfo<Task> pageInfo = new PageInfo<>(tasks);
+            result.setResult(pageInfo);
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
             throw new RuntimeException(e);
@@ -308,61 +388,4 @@ public class TaskManagerServiceImpl implements TaskManagerService {
         return result;
     }
 
-    /**
-     * @author: zlh
-     * @param: taskManager 任务修改内容
-     * @description: 编辑任务
-     * 权限：只有任务的创建人可以进行编辑；
-     * @date: 17:05 2018/4/13
-     */
-    @Override
-    public ExecuteResult<String> updateTaskByTask(Task task) {
-        ExecuteResult<String> result = new ExecuteResult<String>();
-        try {
-            // check参数
-            if (task == null) {
-                result.addErrorMessage("参数传入有误");
-                return result;
-            }
-
-            // 检查权限
-            Task task2 = taskMapper.queryTaskById(task.getId());
-            String createUserName = task2.getCreateUser().getUsername();
-            if (!"当前登录用户名".equals(createUserName)) {
-                /*return 没有权限*/
-                result.setResult("没有权限");
-                return result;
-            }
-            taskMapper.updateTaskById(task);
-            result.setResult("修改成功");
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return result;
-    }
-
-    /**
-     * @author: zlh
-     * @param: taskManager 任务修改内容
-     * @description: 需求是否变更
-     * @date: 17:37 2018/4/13
-     */
-    @Override
-    public ExecuteResult<String> updateDemandChangeByTask(Task task) {
-        ExecuteResult<String> result = new ExecuteResult<String>();
-        try {
-            // check参数
-            if (task == null) {
-                result.addErrorMessage("参数传入有误");
-                return result;
-            }
-            taskMapper.updateTaskById(task);
-            result.setResult("修改成功");
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
-        return result;
-    }
 }
