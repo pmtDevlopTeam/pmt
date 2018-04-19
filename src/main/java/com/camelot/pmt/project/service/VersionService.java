84329379e0e8eb470ec8043cccef0326035f2341
mztyd <17090023169@163.com>
2018-04-19 18:00:38 +0800

    update

--- src/main/java/com/camelot/pmt/project/service/VersionService.java
+++ src/main/java/com/camelot/pmt/project/service/VersionService.java
@@ -32,7 +32,7 @@ public interface VersionService {
      * @author: xueyj
      * @date: 2018/4/13 19:14
      */
-    boolean deleteVersionById(String userId, Long versionId);
+    boolean updateVersionByIdAndParms(String versionStatus,String userId, Long versionId);
 
     /**
      * @Description: 根据versionid查询version信息
