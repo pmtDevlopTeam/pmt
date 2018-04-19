507e9898e32620d58efac21272aef3a4bfa1ffd5
zz <1798805612@qq.com>
2018-04-19 17:27:15 +0800

    产出物代码优化

--- src/main/java/com/camelot/pmt/filemanage/mapper/FileManageGroupMapper.java
+++ src/main/java/com/camelot/pmt/filemanage/mapper/FileManageGroupMapper.java
@@ -23,7 +23,7 @@ public interface FileManageGroupMapper {
 
     List<FileManageGroup> querytFileGroup(FileManageGroup fileManageGroup);//根据不同条件查询（parentID，projectID）
 
-    List<Long> selectFileManagerGroupByParentId(Long id);//根据父级ID进行查询
+    List<Long> queryFileManagerGroupByParentId(Long id);//根据父级ID进行查询
 
 
 }
