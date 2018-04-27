a75fbb5159cbd0ef488898b8efa17983db1f447f
956772658@qq.com <956772658@qq.com>
2018-04-27 16:03:35 +0800

    提交合并4组

--- src/main/java/com/camelot/pmt/platform/service/SchedulerTask.java
+++ src/main/java/com/camelot/pmt/platform/service/SchedulerTask.java
@@ -1,7 +1,6 @@
 package com.camelot.pmt.platform.service;
 
 /**
- * 
  * @author gnerv
  * @Description 定时任务服务接口
  * @date 2018年4月18日
@@ -9,3 +8,4 @@ package com.camelot.pmt.platform.service;
 public interface SchedulerTask {
 
 }
+
